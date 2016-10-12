a22833a4f071e1e2a287fd323ee70c0230614409
Lyle Garza <morezero06@gmail.com>
2016-10-11 22:20:55 -0500

    remove file

--- test.js
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer'
-import Link from ./link
-  it('default snapshot for Link', () => {
-    const tree = renderer.create(
-      <Link  page = "abcdefghijklmnopqrstuvwxyz">abcdefghijklmnopqrstuvwxyz</Link>
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
