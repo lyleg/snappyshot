54841cbfb9c0bc1f6a11d56c87b017a4b3800630
Lyle Garza <morezero06@gmail.com>
2016-10-11 22:02:22 -0500

    some tests

---
+++ src/__tests__/utils.js
@@ -0,0 +1,14 @@
+import {computeFileDepth, generateFilePathTraversal} from '../utils.js'
+
+
+test('computes proper file depth', ()=>{
+  expect(computeFileDepth('foo.js')).toBe(0)
+  expect(computeFileDepth('foo/foo.js')).toBe(1)
+  expect(computeFileDepth('foo/bar/foo.js')).toBe(2)
+})
+
+test('generate proper traversal', ()=>{
+  expect(generateFilePathTraversal('foo.js')).toBe('../')
+  expect(generateFilePathTraversal('foo/foo.js')).toBe('../../')
+  expect(generateFilePathTraversal('foo/bar/foo.js')).toBe('../../../')
+})
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 
 
 export function generateFilePathTraversal(filePath:string){
-  let depth = computeFileDepth(filePath)
+  let depth = computeFileDepth(filePath) + 1 //add one to get out of tests folder
   return [...Array(depth)].reduce(filePathTraversal => filePathTraversal + '../','')
 }
 
-function computeFileDepth(filePath:string):number{
-  return filePath.split('/').length
+export function computeFileDepth(filePath:string):number{
+  return filePath.split('/').length - 1
 }
