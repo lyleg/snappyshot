e5e1338f3614458e663a1426b5dda0e51871d651
Lyle Garza <morezero06@gmail.com>
2017-01-08 11:26:10 -0600

    recursive readdir progress

--- src/snappyshot-cli.js
+++ src/snappyshot-cli.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node --harmony
 /* @flow */
+
+/*
+ default - scaffolds files with placeholders
+ min - just scaffolds the __tests__ files with the subject imported
+ guess - generates input from flow and proptype
+*/
+
 var argv = require('nomnom')
   .script('snappyshot')
   .help('help content')
@@ -20,24 +27,43 @@ var argv = require('nomnom')
   let generateSnapshot = require('../dist/index').generateSnapshot
   let {isDir, computeFolderPath} = require('../dist/utils')
   let mkdirp = require('mkdirp')
+  let recursive = require('recursive-readdir')
+  let to = require('await-to-js').default
+
+  let readFilePromise = toPromise(fs.readFile)
+
+function ignoreFunc(file, stats) {
+  if(file.indexOf('test') !== -1){
+    return true
+  }else{
+    return path.extname(file) !== '.js'
+  }
+}
 
 
+async function parseFiles(err, files:Array<string>){
+  files.forEach(async (filePath)=>{
+    let [fileReadErr,fileSrc] = await to(readFilePromise(filePath, { encoding: 'UTF-8' }))
+    if(fileReadErr){
+      console.log(fileReadErr)
+      return
+    }
+     let snapshot = generateSnapshot(fileSrc, filePath)
+     let writeFilePath = '__tests__/' +  filePath
+     let writeFolderPath = computeFolderPath(writeFilePath)
 
+     if(!await isDir(writeFolderPath)){
+       await toPromise(mkdirp)(writeFolderPath)
+     }
 
-  async function readFiles(filePath:string){//convert to async / await and handle when file doesn't exit
-    try{
-      let fileSrc = await toPromise(fs.readFile)(filePath, { encoding: 'UTF-8' })
-      let snapshot = generateSnapshot(fileSrc, filePath)
-      let writeFilePath = '__tests__/' +  filePath
-      let writeFolderPath = computeFolderPath(writeFilePath)
-
-      if(!await isDir(writeFolderPath)){
-        await toPromise(mkdirp)(writeFolderPath)
-      }
-
-      await toPromise(fs.writeFile)(writeFilePath, snapshot)
-    }catch(e){
-      console.log(e)
+     let [writeFileErr, writeFile] = await to(toPromise(fs.writeFile)(writeFilePath, snapshot))
+  })
+}
+  async function readFiles(targetPath:string){
+    if(await isDir(targetPath)){
+      recursive(targetPath, [ignoreFunc],parseFiles)
+    }else{
+      parseFiles(false,[targetPath])
     }
  }
 
