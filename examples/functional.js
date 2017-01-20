5f524cd15d8343dabd4f5232f1b090aecf9be79f
Lyle Garza <morezero06@gmail.com>
2017-01-19 21:41:20 -0600

    prettier

--- examples/functional.js
+++ examples/functional.js
@@ -1,20 +1,12 @@
 /* @flow */
 
-type Name = {
-  first:string,
-  last:string
-}
+type Name = { first: string, last: string };
 
-type Address = {
-  name:Name,
-  street:string,
-  zipcode: number
-}
+type Address = { name: Name, street: string, zipcode: number };
 
 export function add(num1: number, num2: number): number {
   return num1 + num2;
 }
-
 
 export function total(numbers: Array<number>) {
   var result = 0;
@@ -24,6 +16,6 @@ export function total(numbers: Array<number>) {
   return result;
 }
 
-export function processCustomFlowType(address:Address, foo:string){
-  return address
+export function processCustomFlowType(address: Address, foo: string) {
+  return address;
 }
--- examples/link.js
+++ examples/link.js
@@ -1,46 +1,38 @@
 //may need to remove this before next npm release, not sure about license
-import React from 'react';
+import React from "react";
 
-const STATUS = {
-  NORMAL: 'normal',
-  HOVERED: 'hovered',
-};
+const STATUS = { NORMAL: "normal", HOVERED: "hovered" };
 
-type Props = {
-  page: string,
-  children: string
-}
+type Props = { page: string, children: string };
 export class Link extends React.Component {
-  props:Props
+  props: Props;
   constructor() {
     super();
 
     this._onMouseEnter = this._onMouseEnter.bind(this);
     this._onMouseLeave = this._onMouseLeave.bind(this);
 
-    this.state = {
-      class: STATUS.NORMAL,
-    };
+    this.state = { class: STATUS.NORMAL };
   }
 
   _onMouseEnter() {
-    this.setState({class: STATUS.HOVERED});
+    this.setState({ class: STATUS.HOVERED });
   }
 
   _onMouseLeave() {
-    this.setState({class: STATUS.NORMAL});
+    this.setState({ class: STATUS.NORMAL });
   }
 
   render() {
     return (
       <a
         className={this.state.class}
-        href={this.props.page || '#'}
+        href={this.props.page || "#"}
         onMouseEnter={this._onMouseEnter}
-        onMouseLeave={this._onMouseLeave}>
+        onMouseLeave={this._onMouseLeave}
+      >
         {this.props.children}
       </a>
     );
   }
-
 }
